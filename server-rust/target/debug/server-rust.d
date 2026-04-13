7238d04c19977f51ff86b6aa1d5fca99da2ed580
bolczakk <olegswierblewski@gmail.com>
2026-04-14 01:03:35 +0200

    Revert "first commit"
    This reverts commit 366fc10631722b9d508506c5743594d873836a00.

--- server-rust/target/debug/server-rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Oleg\developer\vw\server-rust\target\debug\server-rust.exe: C:\Users\Oleg\developer\vw\server-rust\src\client.rs C:\Users\Oleg\developer\vw\server-rust\src\main.rs
---
+++ src/Camera.h
@@ -0,0 +1,36 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+enum CAMDIR {
+	FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN
+};
+
+class Camera {
+public:
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+
+    float radius;
+
+    float Yaw = -90.0f;
+    float Pitch = 0.0f;
+    float MovementSpeed = 10.0f;
+    float MouseSensitivity = 0.1f;
+    float Zoom = 45.0f;
+    Camera(glm::vec3 position, glm::vec3 up=glm::vec3(0.0f, 1.0f, 0.0f), float yaw=-90.0f, float pitch=0.0f);
+    void ProcessMouseScroll(float yoffset);
+    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+    void ProcessKeyboard(CAMDIR direction, float deltaTime);
+    glm::mat4 GetViewMatrix();
+private:
+    void updateCameraVectors();
+};
+
+#endif
